5864a5dc4482ae5918b58b0575c5e9f0f2167b49
John ELias <jkmasterpiece@gmail.com>
2026-06-29 18:52:51 +0300

    initial commit

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Error = () => {
+  return (
+    <h4 className='section section-center text-center'>
+      <h2>there was an error...</h2>
+    </h4>
+  );
+};
+
+export default Error;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa natus
+            rerum et ut animi, aliquid voluptas debitis modi adipisci facere, ex
+            eos quos voluptatem necessitatibus provident deleniti iure
+            reprehenderit earum molestiae? Natus sit aliquam inventore iste
+            reprehenderit voluptas iusto laboriosam id! Culpa obcaecati
+            molestiae eligendi rem! Similique necessitatibus neque hic.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
